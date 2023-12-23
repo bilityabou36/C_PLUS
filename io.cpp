35329368bad0e58f32f2974b4542d1f5a7f2e99b
abou bility <bilityabou36@gmail.com>
2023-12-23 01:06:37 -0500

    day

--- io.cpp
+++ io.cpp
@@ -12,26 +12,5 @@ file.close();
 
 
 
-reading from file
-int main(){
-    fstream file ("hello.text");
-    vector<string> names;
 
-    sting input;
-
-    while(file >> input){
-        name.push_back(input);
-    }
-    for (name : names){
-        std::cout << name << std::endl;
-    }
-}
-
-
-// 
-
-fstream file ("hello.txt");
-string line
-getline(file, line)
-cout << file, line
 
